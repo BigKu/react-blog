ca7170426a07feea409334f1dc76682c4eb2c312
sangmo <rntkdah2005@gmail.com>
2019-06-09 19:02:48 +0900

    react-blog init
    1. 템플릿 생성
    2. Header, Footer 컴포넌트 생성

---
+++ src/frontend/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import Root from 'client/Root';
+
+import './index.css';
+
+ReactDom.render(
+    <Root />,
+    document.getElementById('root')
+);
