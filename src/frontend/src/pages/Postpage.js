5e73c4baec0b84de047fa5de5702c2d8cc3cb2ab
sangmo <smk009@naver.com>
2019-06-22 17:49:31 +0900

    Post, Pagination, PostList 컴포넌트 추가

--- src/frontend/src/pages/Postpage.js
+++ src/frontend/src/pages/Postpage.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 
+import { PageTemplate } from 'components/common';
+import { Post } from 'components/post';
+
 class Postpage extends Component {
     render() {
         return (
-            <>
-                Postpage
-            </>
+            <PageTemplate>
+                <Post title="제목1" date="2018-02-14" body="본문 내용" />
+            </PageTemplate>
         );
     }
 }
